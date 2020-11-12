b77a7111734b8244e999ba7e00a6657145d5febc
Andrew Woodyatt <andrew.woodyatt@pacom.com>
2020-11-12 20:12:18 +1100

    Hopefully improved default nav display in small screens.

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
 
-  public navbarCollapsed: boolean = false;
+  public navbarCollapsed: boolean = true;
 
   constructor() { }
 
